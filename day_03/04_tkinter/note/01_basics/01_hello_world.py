5e09295c7103c78397652fca9b19c3dcd406b19a
aelocarinasCAB <locarinasangelinecab@gmail.com>
2025-11-15 18:51:18 +0800

    additional

--- day_03/04_tkinter/note/01_basics/01_hello_world.py
+++ day_03/04_tkinter/note/01_basics/01_hello_world.py
@@ -1,11 +1,12 @@
 import tkinter
 
 root = tkinter.Tk()
+
 root.title("Sample GUI Application")
 root.geometry("1200x400")
 
 label = tkinter.Label(root, text="Hello")
-label.pack()
+label.pack(side='left')
 
 next_label = tkinter.Label(root, text="World")
 next_label.pack()
